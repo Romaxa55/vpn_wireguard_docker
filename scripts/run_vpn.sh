f595f7d0b5bdd5b9a17262ab7679a02357bab29f
Roman SHamagin <roman.shamagin@7vpn.com>
2023-06-06 17:58:33 +0300

    add gui

--- scripts/run_vpn.sh
+++ scripts/run_vpn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose -f docker-compose_wireguard.yaml up -d
+#docker-compose -f docker-compose_wireguard.yaml up -d
+docker stack deploy --compose-file docker-compose_wireguard.yaml wireguard
