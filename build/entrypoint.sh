11c1071de9a460564c2a27d43ed5f139624b924e
Roman SHamagin <roman.shamagin@7vpn.com>
2023-06-06 19:28:40 +0300

    add gui

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -e
 
-# Запускать WireGuard как обычно
-/init &
+# Запускать процесс слежения в фоне
+(
+    while inotifywait -e modify /etc/wireguard/wg0.conf; do
+        echo "Конфигурационный файл WireGuard был изменен, перезапуск WireGuard."
+        wg-quick down wg0
+        wg-quick up wg0
+    done
+) &
 
-# Запускать процесс слежения за изменениями в конфигурационном файле.
-while inotifywait -e modify /config/wg0.conf; do
-    echo "Конфигурационный файл WireGuard был изменен, перезапуск WireGuard."
-    wg-quick down wg0
-    wg-quick up wg0
-done
+# Запускать WireGuard как обычно
+exec /init
