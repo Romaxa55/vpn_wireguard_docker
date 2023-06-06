c64115cb7491276018333900cf46be05f495f94f
Roman SHamagin <roman.shamagin@7vpn.com>
2023-06-07 02:22:48 +0300

    add ansible role

--- scripts/run_traefik.sh
+++ scripts/run_traefik.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker stack deploy --compose-file docker-compose_traefik.yaml traefik
+#docker stack deploy --compose-file docker-compose_traefik.yaml traefik
+docker-compose -f docker-compose_traefik.yaml up -d
