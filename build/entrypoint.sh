0c35978a76f07abca767cc11db22593a9ffe6e94
Roman SHamagin <roman.shamagin@7vpn.com>
2023-06-29 21:09:30 +0300

    update config save

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 
-# Запускать процесс слежения в фоне
-(
-    while inotifywait -e modify /config/wg0.conf; do
-        echo "Конфигурационный файл WireGuard был изменен, перезапуск WireGuard."
-        wg-quick down wg0
-        wg-quick up wg0
-    done
-) &
+## Запускать процесс слежения в фоне
+#(
+#    while inotifywait -e modify /config/wg0.conf; do
+#        echo "Конфигурационный файл WireGuard был изменен, перезапуск WireGuard."
+#        wg-quick down wg0
+#        wg-quick up wg0
+#    done
+#) &
 
 # Запускать wg-api в фоне и перезапускать, если он завершается с ошибкой
 (
